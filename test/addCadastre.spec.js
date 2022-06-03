fa5d7a2a308bf38b7db9aac6bcfd25a1aca3c9df
Lucas <43614096+Twistere@users.noreply.github.com>
2022-06-03 20:39:53 +0200

    test: add selenium test

--- test/addCadastre.spec.js
+++ test/addCadastre.spec.js
@@ -1,26 +1,27 @@
 const { Builder, By} = require("selenium-webdriver");
 const chrome = require('selenium-webdriver/chrome');
+const assert = require('assert')
 
 async function addCadastreTest() {
     const options = new chrome.Options()
     options.setAcceptInsecureCerts(true)
     let driver = await new Builder()
         .forBrowser("chrome")
+        .usingServer('http://127.0.0.1:4444/wd/hub/')
         .setChromeOptions(options)
-        .usingServer("http://127.0.0.1:4444/wd/hub/")
         .build();
     
-    await driver.get("http://127.0.0.1:3000")
+    await driver.get("http://127.0.0.1:8080/");
     await driver.findElement(By.id("fit")).click()
+
     assert(await driver.switchTo().alert().getText() == "Quel est le nom de votre commune ?")
     {
-        const alert = await driver.switchTo().alert()
-        await alert.sendKeys("Lons le saunier")
-        await alert.accept()
+      const alert = await driver.switchTo().alert()
+      await alert.sendKeys("Lons le Saunier")
+      await alert.accept()
+
     }
-
-    driver.quit();
-
+    await driver.quit();
 }
+addCadastreTest();
 
-addCadastreTest()
